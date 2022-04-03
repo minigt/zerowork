0e642d6f1271fba86d84124badd9ce32d9c2b4b9
minigt <minigtraverso@gmail.com>
2022-04-03 18:42:02 -0300

    Added error codes

--- src/main/java/ar/com/minigt/zerowork/todoapi/exceptions/TodoErrorCode.java
+++ src/main/java/ar/com/minigt/zerowork/todoapi/exceptions/TodoErrorCode.java
@@ -8,7 +8,16 @@ public enum TodoErrorCode {
     DESCRIPTION_NOT_NULL(400, 4003, "Description can't be null"),
     DESCRIPTION_NOT_EMPTY(400, 4004, "Description can't be empty"),
     STATE_NOT_NULL(400, 4005, "State can't be null"),
-    INVALID_STATE(400, 4007, "State is not valid"),
+    INVALID_STATE(400, 4006, "State is not valid"),
+    TODO_ID_CANT_BE_NULL(400, 4007, "Todo id can't be null"),
+    TODO_NOT_FOUND(400, 4008, "Todo not found"),
+    INVALID_TODO_ID(400, 4009, "Invalid todo id"),
+
+    USERNAME_DOESNT_EXISTS(401, 4100, "Username doesn't exist"),
+    USERNAME_ALREADY_EXISTS(401, 4101, "Username already exist"),
+    USERNAME_MUST_BE_SET(401, 4102, "Username cannot be blank"),
+    PASSWORD_MUST_BE_SET(401, 4103, "Password cannot be blank"),
+    PASSWORD_DONT_MATCH(401, 4104, "Passwords don't match"),
     ;
 
     private final int code;
