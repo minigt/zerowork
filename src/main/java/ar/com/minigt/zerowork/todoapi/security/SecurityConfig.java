cfaea6347f324ef2bab4a44be68383b36f8cc491
minigt <minigtraverso@gmail.com>
2022-04-03 18:39:16 -0300

    Added dependencies for token authentication

--- src/main/java/ar/com/minigt/zerowork/todoapi/security/SecurityConfig.java
+++ src/main/java/ar/com/minigt/zerowork/todoapi/security/SecurityConfig.java
@@ -2,10 +2,12 @@ package ar.com.minigt.zerowork.todoapi.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -21,7 +23,13 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
             .csrf().disable()
             .authorizeRequests()
-            .antMatchers("/**").permitAll();
+            .antMatchers(HttpMethod.POST, "/users/register").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .addFilter(new AuthenticationFilter(authenticationManager()))
+            .addFilter(new AuthorizationFilter(authenticationManager()))
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            ;
     }
 
     @Override
